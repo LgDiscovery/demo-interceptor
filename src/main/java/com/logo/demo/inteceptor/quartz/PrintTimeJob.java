66e413cb73765f61d093eae879a015b2547aca47
liuguang <15779280545@163.com>
2020-03-30 16:37:03 +0800

    添加quartz框架

--- src/main/java/com/logo/demo/inteceptor/quartz/PrintTimeJob.java
+++ src/main/java/com/logo/demo/inteceptor/quartz/PrintTimeJob.java
@@ -11,6 +11,6 @@ public class PrintTimeJob implements Job {
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
         String msg = (String) context.getJobDetail().getJobDataMap().get("msg");
-        System.out.println("current time :"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        System.out.println("current time :"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+"---"+msg);
     }
 }
