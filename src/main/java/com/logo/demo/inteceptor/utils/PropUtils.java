2a0a97e712266c18de8bb605219841e55565b601
liuguang <15779280545@163.com>
2020-04-01 17:53:53 +0800

    读取properties文件

--- src/main/java/com/logo/demo/inteceptor/utils/PropUtils.java
+++ src/main/java/com/logo/demo/inteceptor/utils/PropUtils.java
@@ -1,24 +1,33 @@
 package com.logo.demo.inteceptor.utils;
 
-import org.springframework.util.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class PropUtils {
 
+    private static Logger logger = LoggerFactory.getLogger(PropUtils.class);
+
     private  static Properties properties;
 
+    static {
+        try {
+            readPropertiesFile("config.properties");
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("读取properties文件失败");
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        readPropertiesFile("config.properties");
         System.out.println(String.valueOf(properties.getProperty("username")));
         System.out.println(String.valueOf(properties.getProperty("password")));
         System.out.println(Integer.valueOf(properties.getProperty("age")));
     }
 
-    public static void readPropertiesFile(String filePath) throws Exception {
+    private static void readPropertiesFile(String filePath) throws Exception {
         InputStream resourceAsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath);
         Properties properties = new Properties();
         properties.load(resourceAsStream);
---
+++ src/main/java/com/logo/demo/inteceptor/utils/YmlUtils.java
@@ -0,0 +1,36 @@
+package com.logo.demo.inteceptor.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+import java.util.Map;
+
+public class YmlUtils {
+
+    private static Logger logger = LoggerFactory.getLogger(YmlUtils.class);
+
+    private static Map<String,Object> map;
+
+    static {
+        try {
+            readYmlFile("config.yml");
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("读取yml配置文件失败");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(map.get("username"));
+        System.out.println(map.get("password"));
+        System.out.println(map.get("age"));
+    }
+
+    private static void readYmlFile(String filePath) throws Exception {
+        InputStream resourceAsStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(filePath);
+        Yaml yaml = new Yaml();
+        map = (Map<String, Object>) yaml.load(resourceAsStream);
+    }
+}
